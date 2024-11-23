b687a2deec2471c32eb127f598578cd2f9b422d3
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-22 19:00:35 -0800

    write a function that will verify a string is at least 2 characters long, and slice the string from the second to the second to last character and return as 'new_string'

--- remove_first_and_last_char.py
+++ remove_first_and_last_char.py
@@ -1,7 +1,14 @@
+# Instructions:
+# It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
 my_string = "eloquent"
 
 def remove_char(s):
-    for i in range(len(s)):
-        print (f"Index {i}: {s[i]}")
-
+    if len(s) < 2:
+        print("Error: Cannot process string less than 2 characters in length.")
+    else:
+        new_string = my_string[1:-1]
+            
+        print(new_string)
+    
 remove_char(my_string)
